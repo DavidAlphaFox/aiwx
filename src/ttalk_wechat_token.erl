e1af072cec0b6b0d48dba24b88dd4c9004aa873f
David.Gao <david.alpha.fox@gmail.com>
2018-04-10 17:14:17 +0800

    format code

--- src/ttalk_wechat_token.erl
+++ src/ttalk_wechat_token.erl
@@ -6,37 +6,34 @@
 
 -spec request_access_token(AppID::binary(),Secret::binary()) -> list().
 request_access_token(AppID,Secret)->
-		URL = hackney_url:make_url(?WECHAT_API_HOST,?WECHAT_ACCESS_TOKEN_PATH,
-															 [{<<"grant_type">>,<<"client_credential">>},
-																{<<"appid">>,AppID},
-																{<<"secret">>,Secret}]),
-		Body = do_api_get(URL),
-		jsx:decode(Body).
+    URL = hackney_url:make_url(?WECHAT_API_HOST,?WECHAT_ACCESS_TOKEN_PATH,
+			       [{<<"grant_type">>,<<"client_credential">>},{<<"appid">>,AppID},{<<"secret">>,Secret}]),
+    Body = do_api_get(URL),
+    jsx:decode(Body).
     
 -spec request_ticket(AccessToken::binary(),Type::binary()) -> list().
 request_ticket(AccessToken,Type)->
-		URL = hackney_url:make_url(?WECHAT_API_HOST,?WECHAT_TICKET_PATH,
-															 [{<<"type">>,Type},
-																{<<"access_token">>,AccessToken}]),
-		Body = do_api_get(URL),
+    URL = hackney_url:make_url(?WECHAT_API_HOST,?WECHAT_TICKET_PATH,
+			       [{<<"type">>,Type},{<<"access_token">>,AccessToken}]),
+    Body = do_api_get(URL),
     jsx:decode(Body).
     
 		
 -spec request_jsapi_ticket(AccessToken::binary()) -> list().
 request_jsapi_ticket(AccessToken)->
-		request_ticket(AccessToken,<<"jsapi">>).
+    request_ticket(AccessToken,<<"jsapi">>).
 
 -spec request_card_ticket(AccessToken::binary()) -> list().
 request_card_ticket(AccessToken)->
-		request_ticket(AccessToken,<<"wx_card">>).
+    request_ticket(AccessToken,<<"wx_card">>).
 
 %%%
 %%%  priavte functions 
 %%%
 -spec do_api_get(URL::binary()) -> binary().
 do_api_get(URL)->
-		Method = get,
+    Method = get,
     Headers = [{<<"Content-Type">>, <<"application/json">>}],
     {ok, _StatusCode, _RespHeaders, ClientRef} = hackney:request(Method, URL, Headers),
     {ok, Body} = hackney:body(ClientRef),
-		Body.
+    Body.
